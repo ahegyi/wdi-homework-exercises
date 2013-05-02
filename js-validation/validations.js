2e43b5499757190854848dc7baca17f9babcb4b7
Aron Hegyi <ahegyi@gmail.com>
2013-05-02 11:40:13 -0700

    implemented validators

--- js-validation/validations.js
+++ js-validation/validations.js
@@ -27,3 +27,63 @@ function validateEmail (email){
   var regEx = /\b[^@]+@[^@]+\.[^@]+\b/;
   return regEx.test(email);
 }
+
+$(document).ready( function () {
+
+  $('input').on('blur', function() {
+    handleError($(this).parent());
+  });
+
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+  });
+
+});
+
+// type is the kind of input field to validate
+// valid is whether or not
+function handleError(block) {
+  var inputField = block.children("input");
+  var entry = inputField.val();
+  var type = inputField.attr("id");
+  var valid = false;
+  var currentError = "";
+
+  console.log(inputField);
+
+  var errors = {
+    "name" : "Your name must be at least 3 characters long.",
+    "age" : "Please enter a valid age (whole numbers only!)",
+    "phone" : "Please enter a ten digit North American phone number.",
+    "email" : "This is not a valid e-mail address - check your typing!"
+  };
+
+  console.log("type");
+
+  switch (type) {
+    case "name":
+      valid = validateName(entry);
+      break;
+    case "age":
+      valid = validateAge(entry);
+      break;
+    case "phone":
+      valid = validatePhone(entry);
+      break;
+    case "email":
+      valid = validateEmail(entry);
+      break;
+  }
+
+  console.log(valid);
+
+  block.children('span.error').remove();
+
+  if (!valid) {
+    block.append('<span class="error">' + errors[type] + '</span>');
+  }
+  // else {
+  //   block.children('span.error').remove();
+  // }
+
+}
