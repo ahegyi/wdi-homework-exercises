d7c2ca211966f80a7b0e3d317580b3845433d812
Aron Hegyi <ahegyi@gmail.com>
2013-05-02 10:55:17 -0700

    Add validatePhone function for js-validations

--- js-validation/validations.js
+++ js-validation/validations.js
@@ -7,3 +7,17 @@ function validateName(name) {
 function validateAge (age){
   return age % 1 === 0 && age > 0 && age < 125;
 }
+
+function validatePhone(phone) {
+  // Thanks to http://regexlib.com/REDetails.aspx?regexp_id=1865 for the NANP compatible regex
+  var phoneRegex = /(?:\([2-9][0-8]\d\)\ ?|[2-9][0-8]\d[\-\ \.\/]?)[2-9]\d{2}[- \.\/]?\d{4}\b/;
+
+  var phoneDigits = phone.replace(/[\D]/g,"");
+
+  if (typeof(phone) === "string" && phoneRegex.test(phoneDigits)) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
